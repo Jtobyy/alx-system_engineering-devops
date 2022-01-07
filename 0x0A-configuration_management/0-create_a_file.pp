4f0144f426d3becf6451fcdccf9a7db35a56fd8c
Jtobyy <jtobi8161@gmail.com>
2022-01-07 04:08:05 +0000

    first puppet manifest

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This puppet manifest creates a file in /tmp
+
+file { '/tmp/school':
+  ensure  => file,
+  owner   => www-data,
+  group   => www-data,
+  mode    => '0744',
+  content => 'I love puppet'
+}
