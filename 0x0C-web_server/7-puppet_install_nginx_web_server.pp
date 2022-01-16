a5f3ad85b885f1fadd4c9721054e739e297fe32d
root <root@Botoby.localdomain>
2022-01-17 00:27:16 +0100

    Baby steps

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,26 +4,8 @@ package {'nginx':
   ensure => installed
 }
 
-exec {'index':
-  command => "echo -e 'Hello World!\n' > \
-$(grep -m 1 'root /' /etc/nginx/sites-enabled/default | \
-tr -d ';' | cut -f 1 -d ' ' --complement)/index.html"
+file {'/var/www/html/index.html':
+  ensure => file,
+  content => 'Hello World!\n'
 }
 
-exec {'port':
-  command => sudo sed -i 's/listen [0-9]* default_server/listen 80 default_server/' /etc/nginx/sites-enabled/default
-}
-
-exec {'portipv6':
-  command => sudo sed -i 's/listen \[::\]:[0-9]* \
-default_server ipv6only=on/listen \[::\]:80 default_server \
-ipv6only=on/' /etc/nginx/sites-enabled/default
-}
-
-exec {'redirect':
-  command => sudo sed -i '$(grep -n server_name /etc/nginx/sites-enabled/default | cut -d : -f 1 | head -n 1) i rewrite ^/redirect_me http://jtobyy.tech permanent;' /etc/nginx/sites-enabled/default
-}
-
-exec {'restart':
-  command => sudo service nginx restart
-}
