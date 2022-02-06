0ebe6f835675a3bf0a1f5479f31ea5cf26887754
root <root@Botoby.localdomain>
2022-02-07 00:52:43 +0100

    automate the task of creating a custom HTTP header response with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,13 +11,21 @@ file {'/var/www/html/index.html':
   content => 'Hello World!'
 }
 
+exec {'port':
+  command => "/usr/bin/sed -i 's/listen [0-9]* default_server/listen 80 default_server/' /etc/nginx/sites-enabled/default"
+}
+
+exec {'ipv6port':
+  command => "/usr/bin/sed -i 's/listen [::]:[0-9]* default_server ipv6only=on/listen [::]:80 default_server ipv6only=on/' /etc/nginx/sites-enabled/default"
+}
+
 exec {'newline':
   command => '/usr/bin/sed -i "16  i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf'
 }
 
 exec {'nginx':
   command => '/usr/bin/sudo service nginx restart'
-  }
+}
 
 #  file_line {'Httpsection':
 #  ensure => present,
