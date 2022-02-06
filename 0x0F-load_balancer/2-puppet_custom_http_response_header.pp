a253adfca8b8a804d5abe4d1a5a32f98b504857b
root <root@Botoby.localdomain>
2022-02-07 00:18:26 +0100

    automate the task of creating a custom HTTP header response with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,32 @@
+# This manifest automates the task of
+# setting up a brand new ubuntu machine with nginx and
+# creating a custom HTTP header response, but with Puppet.
+
+# Get puppet stdlib module
+
+mod 'puppetlabs-stdlib', '4.9.1'
+
+package {'nginx':
+  ensure => installed
+}
+
+file {'/var/www/html/index.html':
+  ensure => file,
+  content => 'Hello World!'
+}
+
+exec {'newline':
+  command => '/usr/bin/sed -i "16  i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf'
+}
+
+exec {'nginx':
+  command => '/usr/bin/sudo service nginx restart'
+  }
+
+#  file_line {'Httpsection':
+#  ensure => present,
+#  path => '/etc/nginx/nginx.conf',
+#  line => 'add_header X-Served-By \$hostname;',
+#  after => 'http {'
+#}
+
