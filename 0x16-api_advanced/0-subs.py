f41471dbe181613ee37de6abca3a5b35d2a4e55a
root <root@Botoby.localdomain>
2022-02-16 03:58:23 +0100

    Works for invalid subreddits

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""Queries the Reddit API and returns the number of
+subscribers (not active users,total subscribers) for a given subreddit.
 """
-Task 0
-"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    pass
+    r = requests.get('https://reddit.com/r/programming/',
+                     allow_redirects=False)
+    if r.status_code != 200:
+        return 0
+    return len(r)
